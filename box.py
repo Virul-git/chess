fbec48418561e0fea0b594cefccc038ea6bbc0ed
Virul-git <katlavirul.vk@gmail.com>
2017-06-12 22:49:48 +0000

    board frame complete

--- box.py
+++ box.py
@@ -2,8 +2,8 @@ import numpy
 import pygame 
 
 
-display_width = 1024
-display_height = 768
+display_width = 420
+display_height = 420
 	
 black = (0,0,0)
 red = (255,0,0)
@@ -23,17 +23,36 @@ def input_from_user():
 			quit()
 
 def drawBox(gameDisplay):
-	pygame.draw.line(gameDisplay,blue,(100,100),(100,900))
-	pygame.draw.line(gameDisplay,blue,(100,900),(900,900))
-	pygame.draw.line(gameDisplay,blue,(900,900),(900,100))
-	pygame.draw.line(gameDisplay,blue,(900,100),(100,100))
+	################## BASIC BOX	####################
+	pygame.draw.line(gameDisplay,blue,(10,10),(10,410))
+	pygame.draw.line(gameDisplay,blue,(10,410),(410,410))
+	pygame.draw.line(gameDisplay,blue,(410,410),(410,10))
+	pygame.draw.line(gameDisplay,blue,(410,10),(10,10))
+	#################	INTERNAL LINES	#################
+	pygame.draw.line(gameDisplay,blue,(60,10),(60,410))
+	pygame.draw.line(gameDisplay,blue,(110,10),(110,410))
+	pygame.draw.line(gameDisplay,blue,(160,10),(160,410))
+	pygame.draw.line(gameDisplay,blue,(210,10),(210,410))
+	pygame.draw.line(gameDisplay,blue,(260,10),(260,410))
+	pygame.draw.line(gameDisplay,blue,(310,10),(310,410))
+	pygame.draw.line(gameDisplay,blue,(360,10),(360,410))
+	################## HORIZONTAL ####################
+	pygame.draw.line(gameDisplay,blue,(410,60),(10,60))
+	pygame.draw.line(gameDisplay,blue,(410,110),(10,110))
+	pygame.draw.line(gameDisplay,blue,(410,160),(10,160))
+	pygame.draw.line(gameDisplay,blue,(410,210),(10,210))
+	pygame.draw.line(gameDisplay,blue,(410,260),(10,260))
+	pygame.draw.line(gameDisplay,blue,(410,310),(10,310))
+	pygame.draw.line(gameDisplay,blue,(410,360),(10,360))
+	################# VERTICAL	######################
 
-def hero(xy):
-	
+def hero(xy,gameDisplay):
+	pygame.draw.rect(gameDisplay,blue,xy,20,20)	
 
 def gameloop():
 	gamerunning = True
 	pixAr = pygame.PixelArray(gameDisplay)
+	xy = (200,200)
 	while gamerunning:
 		drawBox(gameDisplay)
 		input_from_user()
